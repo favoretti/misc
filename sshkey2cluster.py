3de5399f8612e7b1000e18431343115d30aa87f0
Vladimir Lazarenko <favoretti@gmail.com>
2012-09-28 00:13:47 +0200

    Added some more formatting ;)

--- sshkey2cluster.py
+++ sshkey2cluster.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
-import argparse
 import sys
-import pexpect
+try:
+    import argparse
+except:
+    print("*** Please install 'argparse' for me to work")
+    sys.exit(1)
+try:
+    import pexpect
+except:
+    print("*** Please install 'pexpect' for me to work")
+    sys.exit(1)
 import getpass
 import os
 
@@ -18,16 +26,16 @@ def check_key(host, id_key):
         '.*nodename nor servname provided.*',
         '.*forward host lookup failed.*'], timeout=4 )
     if a == 0:
-        print("Your key is already present.")
+        print("[:)] Your key is already present.")
         return True
     elif a == 1 or a == 2 or a == 3 or a == 5 or a == 6:
-        print("You key wasn't found.")
+        print("[ii] You key wasn't found.")
         return False
     elif a == 7 or a == 8:
-        print("Host not found.")
+        print("[!!] Host not found.")
         return True # hackety hack
     elif a == 4:
-        print("Timout occured.")
+        print("[!!] Timout occured.")
         return True
 
 
@@ -40,12 +48,12 @@ def main():
     args = parser.parse_args()
 
     if not args.hostlist and not args.host:
-        print("Whatcha want, buddy?")
+        print("[??] Whatcha want, buddy?")
         parser.print_usage()
         sys.exit(0)
 
     if args.hostlist and args.host:
-        print("You can't have both hostlist and host. Pick one")
+        print("[!!] You can't have both hostlist and host. Pick one")
         parser.print_usage()
         sys.exit(0)
 
@@ -56,7 +64,7 @@ def main():
             hosts = [line.strip() for line in open(args.hostlist)]
 
         except IOError:
-            print("File {} can't be found.".format(args.hostlist))
+            print("[!!] File {} can't be found.".format(args.hostlist))
             sys.exit(1)
 
     if args.host:
@@ -65,19 +73,19 @@ def main():
     try:
         id_key = open(args.key, 'r').read().strip()
     except IOError:
-        print("Public key file {} can't be found.".format(args.key))
+        print("[!!] Public key file {} can't be found.".format(args.key))
         sys.exit(1)
 
     try:
         password = getpass.getpass('Type password in. We shall asssume it is the same for everything: ')
         for host in hosts:
-            print("Processing {}".format(host))
+            print("[!!] Processing {}".format(host))
 
             if check_key(host, id_key):
                 continue
 
             command = 'echo {0} | /usr/bin/ssh {1} "umask 077; test -d .ssh || mkdir .ssh ; cat >> .ssh/authorized_keys"'.format(id_key, host)
-            print("Placing key...")
+            print("[ii] Placing key...")
             child = pexpect.spawn('/bin/bash', ['-c', command])
             ret = child.expect( [ 'Are you sure you want to continue connecting.*',
                 '.*assword:',
@@ -85,43 +93,43 @@ def main():
                 pexpect.EOF, pexpect.TIMEOUT,
                 'Password:' ], timeout=4 )
             if ret == 0:
-                print("Accepting hostkey")
+                print("[++] Accepting hostkey")
                 child.sendline("yes")
                 b = child.expect([ '.*assword:', pexpect.EOF, pexpect.TIMEOUT ], timeout=10)
                 if b == 0:
-                    print("Sending password")
+                    print("[--] Sending password")
                     child.sendline(password)
                     c = child.expect([ '.*assword:', pexpect.EOF, pexpect.TIMEOUT ], timeout=10)
                     if c == 0:
-                        print("Login failed.")
+                        print("[!!] Login failed.")
                         continue
                     elif c == 2:
-                        print("Timeout occured.")
+                        print("[!!] Timeout occured.")
                         continue
                     else:
-                        print("Key placed.")
+                        print("[:)] Key placed.")
             elif ret == 1 or ret == 5:
-                print("Sending password")
+                print("[--] Sending password")
                 child.sendline(password)
                 b = child.expect([ '.*assword:', pexpect.EOF, pexpect.TIMEOUT ], timeout=10)
                 if b == 0:
-                    print("Login failed.")
+                    print("[!!] Login failed.")
                     continue
                 elif b == 2:
-                    print("Timeout occured.")
+                    print("[!!] Timeout occured.")
                     continue
                 else:
-                    print("Key placed.")
+                    print("[:)] Key placed.")
             elif ret == 2:
-                print("Host {} can't be resolved.".format(host))
+                print("[!!] Host {} can't be resolved.".format(host))
             elif ret == 3:
-                print("Key placed.")
+                print("[:)] Key placed.")
             elif ret == 4:
-                print("Connection timeout occured.")
+                print("[!!] Connection timeout occured.")
 
 
     except KeyboardInterrupt:
-        print("Aborted by user.")
+        print("[:|] Aborted by user.")
         sys.exit(0)
 
 if __name__ == '__main__':
